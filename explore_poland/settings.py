f4b8199d7d871c50e6e6f1578bd096597cedb8f3
Karolina <karolina.zagorska1104@gmail.com>
2022-11-18 19:10:49 +0100

    home_page init and base home_page and CSS edit and add form2.html

--- explore_poland/settings.py
+++ explore_poland/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,11 +125,11 @@ STATIC_ROOT = 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = 'explore_media'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = 'media'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = 'places_app/media'
+# MEDIA_URL = '/media/'
+MEDIA_ROOT = 'places_app'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
