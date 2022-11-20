9c6506cba65a3774d857f793abc4cdb7d62d7b38
Karolina <karolina.zagorska1104@gmail.com>
2022-11-20 19:32:17 +0100

    places.css and place.html and base.html ale imagines

--- explore_poland/settings.py
+++ explore_poland/settings.py
@@ -125,11 +125,11 @@ STATIC_ROOT = 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media'
 
-# MEDIA_URL = '/media/'
-MEDIA_ROOT = 'places_app'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'places_app/media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- explore_poland/urls.py
+++ explore_poland/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('', include('home_page_app.urls')),
     path('admin/', admin.site.urls),
     path('test/', include('places_app.urls')),
     path('explore/', include('home_page_app.urls'))
