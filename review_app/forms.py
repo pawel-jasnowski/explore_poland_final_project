5dc59d7c30b27fcb165d234960584e64e45ed7bd
JASNYP <pawel.jasnowski@gmail.com>
2022-11-24 23:19:28 +0100

    commit - user and review app ver1

--- review_app/forms.py
+++ review_app/forms.py
@@ -17,24 +17,49 @@ RATING_CHOICES = [
     ('9', '9'),
     ]
 class CreateReview(ModelForm):
-    pass
-def rating_validator(value):
-    pass
-
-def review_body_validator(value):
-    pass
-
-class ReviewYourReservation(LoginRequiredMixin, ModelForm):
-
     class Meta:
         model = Review
         fields = "__all__"  # all fields from model Review
         # fields = ('rating', 'review_body')
 
-        # widgets = {
-        #     'rating': forms.Select(attrs={'class':'form-control'}, choices=RATING_CHOICES),
-        #     'review_body': forms.Textarea (attrs={'class':'form-control'}),
-        # }
+        widgets = {
+            'rating': forms.Select(attrs={'class':'form-control'}, choices=RATING_CHOICES),
+            'review_body': forms.Textarea (attrs={'class':'form-control'}),
+        }
+
+
+
+
+# RATING_CHOICES = [
+#     ('1', '1'),
+#     ('2', '2'),
+#     ('3', '3'),
+#     ('4', '4'),
+#     ('5', '5'),
+#     ('6', '6'),
+#     ('7', '7'),
+#     ('8', '8'),
+#     ('9', '9'),
+#     ]
+# class CreateReview(ModelForm):
+#     pass
+# def rating_validator(value):
+#     pass
+#
+# def review_body_validator(value):
+#     pass
+#
+# class ReviewYourReservation(LoginRequiredMixin, ModelForm):
+#
+#     class Meta:
+#         model = Review
+#         fields = "__all__"  # all fields from model Review
+#         # fields = ('rating', 'review_body')
+#
+#         # widgets = {
+#         #     'rating': forms.Select(attrs={'class':'form-control'}, choices=RATING_CHOICES),
+#         #     'review_body': forms.Textarea (attrs={'class':'form-control'}),
+#         # }
 
 
 
